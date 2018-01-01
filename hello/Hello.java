2e1198c8aae551b44ac61363ce2d18669d8ca735
jouhs <jaouad.ouhs@gmail.com>
2018-01-01 15:27:06 +0100

    loop while adeded

--- hello/Hello.java
+++ hello/Hello.java
@@ -1,6 +1,8 @@
 package hello;
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello World!!!");
+		for(int i= 1;i<=10; i++){
+			System.out.println("Hello world!!");
+		}
 	}
 }
